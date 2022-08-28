c8151861a591de8866ab925c9553e326bf313417
Max Walley <max.walley1@btinternet.com>
2022-08-28 11:48:47 +0100

    Completed implementation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
+#![allow(non_snake_case)]
+
+use std::io::BufRead;
 use clap::Parser;
+
+#[derive(Debug)]
+struct CustomError(String);
 
 //Custom parser struct that derives from claps parser
 #[derive(Parser)]
@@ -9,6 +15,26 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>>
+{
     let args = Cli::parse();
+
+    let file = std::fs::File::open(&args.path).map_err(|err| format!("Error! Could not read {}, {}", args.path.display(), err))?;
+    let reader = std::io::BufReader::new(file);
+
+    let mut numLinesContainingMsg = 0;
+    for line in reader.lines()
+    {
+        let line = line.map_err(|err| format!("Error! Could not read line, {}", err))?;
+
+        if line.contains(&args.pattern)
+        {
+            numLinesContainingMsg += 1;
+            println!("{}", line);
+        }
+    }
+
+    println!("{} values counted", numLinesContainingMsg);
+
+    Ok(())
 }
